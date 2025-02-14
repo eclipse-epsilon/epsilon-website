aca6d27fbdc56d641a78bec8c7c2b2e528379b6c
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2025-02-14 14:44:26 +0000

    Enabled source maps in webpack

--- mkdocs/docs/playground/webpack.config.js
+++ mkdocs/docs/playground/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map', // Add this line to enable source maps
   module: {
 		rules: [
 			{
